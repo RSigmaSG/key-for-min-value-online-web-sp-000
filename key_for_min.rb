cdca569350469ebda02191201ae1d9caeb477a48
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-19 08:31:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,17 +10,16 @@ def key_for_min_value(name_hash)
   first = true
   
   name_hash.each do |key, num|
-    if (!first)
+    if (first)
       
-      if(lowest > num)
+      lowest = num
+      lowest_key = key
+      
+    elsif (lowest > num)
         lowest = num
         lowest_key = key
         puts "here"
-      end
       
-    else
-      lowest = num
-      lowest_key = key
     end
   end
   return lowest_key
