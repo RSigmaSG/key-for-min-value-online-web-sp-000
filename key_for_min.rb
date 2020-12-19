f794dedbe5bb7ed546ec0db2889322c5708fe61f
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-19 08:30:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,27 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
+  if (name_hash.empty?)
+    return nil
+  end
+  
+  first = true
+  
+  name_hash.each do |key, num|
+    if (!first)
+      
+      if(lowest > num)
+        lowest = num
+        lowest_key = key
+        puts "here"
+      end
+      
+    else
+      lowest = num
+      lowest_key = key
+    end
+  end
+  #return lowest_key
+  
 end
