bed9a7dc090afbc6483b250b05d4fcdd77de2258
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-19 08:34:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,11 +14,11 @@ def key_for_min_value(name_hash)
     if (first)
       
       lowest = num
-      lowest_key << key
+      lowest_key << "#{key}"
       
     elsif (lowest > num)
         lowest = num
-        lowest_key << key
+        lowest_key = key
       
     end
   end
